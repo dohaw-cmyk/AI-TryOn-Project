44cf9e3770539fea82d176ece27d02d0d3a68fde
dohaw-cmyk <kanghyomin0731@gmail.com>
2026-04-04 22:09:58 +0900

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    // !! 주의: 타입 에러가 있어도 빌드를 강제로 완료시킵니다.
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // 빌드 시 ESLint 체크도 무시하게 합니다.
-    ignoreDuringBuilds: true,
-  },
-}
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
